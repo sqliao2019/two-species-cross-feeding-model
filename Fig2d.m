9fbf51ed80341c73d564f4f378acc6ec820b4fe8
sqliao2019 <57887434+sqliao2019@users.noreply.github.com>
2026-07-28 20:32:05 -0500

    Add files via upload

--- Fig2d.m
+++ Fig2d.m
@@ -36,7 +36,7 @@ figure;hold on;
 plot(tb,yb(:,1));
 plot(tp,yp(:,1));
 
-%% 1-D Landscape (normal)
+%% Initial Stage: Landscape (normal) 
 
 figure;hold on;
 
@@ -52,25 +52,19 @@ markerY_1 = interp1(X1D, U1D, markerX_1);
 markerX_2 = yb(end,1);
 markerY_2 = interp1(X1D, U1D, markerX_2);
 
-markerX_3 = yd(end,1);
-markerY_3 = interp1(X1D, U1D, markerX_3);
-
-markerX_4 = ya(end,1);
-markerY_4 = interp1(X1D, U1D, markerX_4);
-
 plot(markerX_1, markerY_1, 'o', 'MarkerEdgeColor','none','MarkerFaceColor','k');
 plot(markerX_2, markerY_2, 'o', 'MarkerEdgeColor','none','MarkerFaceColor','r');
-plot(markerX_3, markerY_3, 'o', 'MarkerEdgeColor','none','MarkerFaceColor','b');
-plot(markerX_4, markerY_4, 'o', 'MarkerEdgeColor','none','MarkerFaceColor','g');
+
 xlim([0 Xmax])
 ylim([min(U1D)-0.2 max(U1D)+0.2])
 box on;
 yticks([]);
 xticks([0 Xmax])
 %xticklabels(["0", "+"])
+title('Initial')
 
 
-%% 1-D Landscape (perturbed)
+%% Pulse Stage: Landscape (perturbed)
 
 figure;hold on;
 
@@ -86,11 +80,61 @@ markerY_2 = interp1(X1D, U1D, markerX_2);
 markerX_3 = yd(end,1);
 markerY_3 = interp1(X1D, U1D, markerX_3);
 
+
+plot(markerX_2, markerY_2, 'o', 'MarkerEdgeColor','none','MarkerFaceColor','r');
+plot(markerX_3, markerY_3, 'o', 'MarkerEdgeColor','none','MarkerFaceColor','b');
+
+xlim([0 Xmax])
+ylim([min(U1D)-0.2 max(U1D)+0.2])
+box on;
+yticks([]);
+xticks([0 Xmax])
+% xticklabels(["0", "+"])
+title('Pulse')
+
+%% Recovery Stage: Landscape (normal)
+
+figure;hold on;
+
+Xmax = Param_normal.Sin/Param_normal.a;
+
+[X1D,U1D] = ode45(@(X,U)Gradient(X,U,Param_normal), linspace(0,Xmax,1000), 0);
+U1D = (U1D-U1D(1))./(max(U1D)-min(U1D));
+plot(X1D, U1D);
+
+markerX_3 = yd(end,1);
+markerY_3 = interp1(X1D, U1D, markerX_3);
+
+markerX_4 = ya(end,1);
+markerY_4 = interp1(X1D, U1D, markerX_4);
+
+plot(markerX_3, markerY_3, 'o', 'MarkerEdgeColor','none','MarkerFaceColor','b');
+plot(markerX_4, markerY_4, 'o', 'MarkerEdgeColor','none','MarkerFaceColor','g');
+xlim([0 Xmax])
+ylim([min(U1D)-0.2 max(U1D)+0.2])
+box on;
+yticks([]);
+xticks([0 Xmax])
+%xticklabels(["0", "+"])
+title('Recovery')
+
+%% Press Stage: Landscape (perturbed)
+
+figure;hold on;
+
+Xmax = Param_perturb.Sin/Param_perturb.a;
+
+[X1D,U1D] = ode45(@(X,U)Gradient(X,U,Param_perturb), linspace(0,Xmax,1000), 0);
+U1D = (U1D-U1D(1))./(max(U1D)-min(U1D));
+plot(X1D, U1D);
+
+markerX_2 = yd(1,1);    
+markerY_2 = interp1(X1D, U1D, markerX_2);
+
 markerX_5 = yp(end,1);
 markerY_5 = interp1(X1D, U1D, markerX_5);
 
 plot(markerX_2, markerY_2, 'o', 'MarkerEdgeColor','none','MarkerFaceColor','r');
-plot(markerX_3, markerY_3, 'o', 'MarkerEdgeColor','none','MarkerFaceColor','b');
 plot(markerX_5, markerY_5, 'o', 'MarkerEdgeColor','none','MarkerFaceColor','m');
 
 xlim([0 Xmax])
@@ -99,3 +143,4 @@ box on;
 yticks([]);
 xticks([0 Xmax])
 % xticklabels(["0", "+"])
+title('Press')
